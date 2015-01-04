8a788e9129e5a397efa9da7ab57ef9ea770b0814
Emily Sas <emily.bronwen.sas@gmail.com>
2015-01-04 16:51:20 +0000

    refactoring to split method into smaller methods

--- lib/inject.rb
+++ lib/inject.rb
@@ -1,20 +1,26 @@
 class Array
-  def my_inject(*args)
-    copy = self.dup
-    if args[0].is_a?(Symbol) || !args.any?
-      memo ||= copy[0] 
-      copy.shift
-    else      
-      memo = args[0]
-    end
-    if args[0].is_a?(Symbol)
-      copy.each {|item| memo = memo.send(args[0], item)}
-    elsif args[1].is_a?(Symbol)
-      copy.each {|item| memo = memo.send(args[1], item)}
-    else 
-      yield memo, copy[1]
-      copy.each {|item| memo = yield memo, item}
-    end  
-    memo
+
+  def my_inject(*args, &block)
+    @copy = self.dup
+    (args[0].is_a?(Symbol) || !args.any?) ? no_other_args(*args) : @memo = args[0]
+    args[0-1].is_a?(Symbol) ? using_symbol(*args) : using_block(*args, &block)
+    @memo
   end
+
+  def no_other_args(*args)
+    @memo ||= @copy[0] 
+    @copy.shift
+  end
+
+  def using_symbol(*args)
+    symbol ||= args[0]
+    symbol = args[1] unless args[1] == nil
+    @copy.each {|item| @memo = @memo.send(symbol, item)}
+  end
+
+  def using_block(*args, &block)
+    @copy.each {|item| @memo = yield @memo, item}
+  end
+
 end
+
